6cc0891b21d439b894d9ab077f9c035e787f8e3f
Nisha Bukke <sononenisha827@gmail.com>
2024-03-20 18:07:57 +0530

    changes

--- script.js
+++ script.js
@@ -56,3 +56,77 @@ $(window).scroll(function() {
 }
 
 });
+
+
+
+document.querySelectorAll('.accordion-title').forEach((accordionToggle) => {
+    accordionToggle.addEventListener('click', () => { 
+      const accordionItem = accordionToggle.parentNode;
+      const accordionContent = accordionToggle.nextElementSibling;
+  
+      // If this accordion item is already open, close it.
+      if (accordionItem.classList.contains('active')) {
+        accordionContent.style.maxHeight = null;
+        accordionItem.classList.remove('active');
+      } else {
+        // Close all other accordion items
+        document.querySelectorAll('.accordion-item').forEach(item => {
+          item.classList.remove('active');
+          const content = item.querySelector('.accordion-content');
+          content.style.maxHeight = null;
+        });
+  
+        // Open this accordion item
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+        accordionItem.classList.add('active');
+      }
+    });
+  });
+
+  
+
+  // product image zoom
+
+//Demo: https://www.codehim.com/demo/zoom-image-on-mouseover-using-javascript/
+let zoomer = function (){
+  document.querySelector('#img-zoomer-box')
+    .addEventListener('mousemove', function(e) {
+
+    let original = document.querySelector('#img-1'),
+        magnified = document.querySelector('#img-2'),
+        style = magnified.style,
+        x = e.pageX - this.offsetLeft,
+        y = e.pageY - this.offsetTop,
+        imgWidth = original.offsetWidth,
+        imgHeight = original.offsetHeight,
+        xperc = ((x/imgWidth) * 100),
+        yperc = ((y/imgHeight) * 100);
+
+    //lets user scroll past right edge of image
+    if(x > (.01 * imgWidth)) {
+      xperc += (.15 * xperc);
+    };
+
+    //lets user scroll past bottom edge of image
+    if(y >= (.01 * imgHeight)) {
+      yperc += (.15 * yperc);
+    };
+
+    style.backgroundPositionX = (xperc - 5) + '%';
+    style.backgroundPositionY = (yperc - 5) + '%';
+
+    style.left = (x - 250) + 'px';
+    style.top = (y - 250) + 'px';
+
+  }, false);
+}();
+
+
+
+// file add
+
+// Add the following code if you want the name of the file appear on select
+$(".custom-file-input").on("change", function() {
+  var fileName = $(this).val().split("\\").pop();
+  $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
